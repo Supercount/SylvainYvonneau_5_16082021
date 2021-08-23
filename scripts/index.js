50cdc8e808918d7902699bc8a0d2ae21d85f9b32
Sylvain Yvonneau <yvonneau.sylvain@gmail.com>
2021-08-23 17:36:52 +0200

    ajout images

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,19 @@
-fetch("http://localhost:3000/api/furniture").then(function(res) {
+const main = document.getElementById("menu");
+
+
+fetch("http://localhost:3000/api/cameras").then(function(res) {
     if (res.ok) {
         return res.json();
     }
 }).then(function(data) {
+    data.forEach(produit => {
+        var newProduct = document.createElement("article");
+        newProduct.classList.add("menu__element");
+        const image_produit = produit.imageUrl;
+        const nom_produit = produit.name;
+        const prix_produit = produit.price;
+        newProduct.innerHTML = "<a href='produit.html'><img src=" + image_produit + " alt='image_produit' class='menu__element__image'><div class='menu__element__description'><p class='menu__element__description__nom'>" + nom_produit + "</p><p class='menu__element__description__prix'>" + prix_produit/100 + " € </p></div></a>";
+        main.appendChild(newProduct);
+    });
     console.log(data);
 })
